e5c1ddb50dbd08d40ae6f3827b437088973808f7
marjorie <marjorie.pezeron@epitech.eu>
2020-03-16 17:03:30 +0100

    [ADD] ajout genre + debut affichage episode

--- myapp/src/components/Series.js
+++ myapp/src/components/Series.js
@@ -11,6 +11,8 @@ class Series extends React.Component {
             show: false,
             modalItems: [],
             pictures: [],
+            episodes: [],
+            show2: false
             // id: '481'
         };
         this.detail = this.detail.bind(this);
@@ -40,27 +42,23 @@ class Series extends React.Component {
                     modalItems: data.show,
                     // id: this.state.id
                 })
-                console.log(data);
-                console.log(data.show.genres.Comedy);
+                //console.log(data);
+                //console.log(data.show.genres.Comedy);
             });
         this.image(id);
     }
 
     image(id) {
         fetch('https://api.betaseries.com/pictures/shows?key=f10eeafae2e6&id=' + id, {
-            method: 'get',
-            headers: {
-                "Content-Type": "image/jpeg"
-            }
+            method: 'get'
         })
-
             .then(data => {
-                console.log(data);
+                //console.log(data);
                 this.setState({
                     pictures: data
                     // id: this.state.id
                 })
-                console.log(data.url);
+                //console.log(data.url);
             });
         this.showModal();
     }
@@ -71,6 +69,29 @@ class Series extends React.Component {
             show: !this.state.show
         });
     }
+
+    episode(id) {
+        fetch(' https://api.betaseries.com/shows/episodes?key=f10eeafae2e6&id=' + id, {
+            method: 'get'
+        })
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    episodes: data.episodes
+                    // id: this.state.id
+                })
+                console.log(data);
+            });
+        this.showModal2();
+    }
+
+    showModal2 = () => {
+        this.setState({
+            ...this.state,
+            show2: !this.state.show
+        });
+    }
+
 
     render() {
         const modalItems = this.state.modalItems;
@@ -86,13 +107,20 @@ class Series extends React.Component {
                                         <img className="img_series" src={item.images.show} alt="visuel séries"></img>
                                         <div className="caption">
                                             <p>{item.title}</p>
-                                            <p><i className="fas fa-star"></i> Note : {item.notes.mean.toLocaleString(undefined, { maximumFractionDigits: 2 })}</p>
+                                            <p><i className="fas fa-star"></i> Note : {item.notes.mean.toLocaleString(undefined, { maximumFractionDigits: 1 })}</p>
+                                            <p>{item.genres.Comedy} {item.genres.Drama} {item.genres.Crime} {item.genres.Horror} {item.genres.Anime} {item.genres.Action} {item.genres.Adventure}
+                                                {item.genres.Fantaisy} {item.genres.Animation} {item.genres.ScienceFiction} {item.genres.Miniseries} {item.genres.Documentary}
+                                                {item.genres.Reality}{item.genres.Romance}{item.genres.Western}{item.genres.TalkShow}{item.genres.GameShow}{item.genres.Thriller}
+                                                {item.genres.Cooked} {item.genres.Soap} {item.genres.Children} {item.genres.Family} {item.genres.Homeandgardening} {item.genres.Mystery}
+                                                {item.genres.News} {item.genres.Specialinterest} {item.genres.Sport} {item.genres.Suspense} {item.genres.Trip} {item.genres.History}
+                                                {item.genres.Indie} {item.genres.Musicalcomedy} {item.genres.Podcast} {item.genres.War} {item.genres.MartialArts}</p>
                                             <script src="https://www.betaseries.com/js/button.js" async></script>
                                             <a href="https://www.betaseries.com" className="btn btn-primary"
                                                 data-type="show"
                                                 data-show={item.title}
                                             >Ajouter la série</a>
                                             <button className='btn btn-info' onClick={() => this.detail(item.id)}>detail série</button>
+                                            <button className='btn btn-info' onClick={() => this.episode(item.id)}>épisodes</button>
                                         </div>
                                     </div>
                                     {/* <button className="btn" value="show modal" onClick={() => this.detail(item.id)}>more</button> */}
@@ -105,6 +133,15 @@ class Series extends React.Component {
                                         <p>Durée d'un épisode : {modalItems.length} min</p>
                                         <p>{modalItems.description}</p>
                                         <button className="btn btn-primary">Archiver la série</button>
+                                    </Modal>
+                                    <Modal show={this.state.show2} onClose={this.showModal2}>
+                                        {this.state.episodes.map((item, index) => (
+                                            <div key={index} className="test">
+                                                {item.code}{item.title}
+                                            </div>
+                                        ))}
+                                        holla
+                                        {/* <h1>{episodes.title}</h1> */}
                                     </Modal>
                                     {/* <button type="button" className="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
                                     <div id="myModal" role="dialog" className="">
